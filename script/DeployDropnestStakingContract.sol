70c163e1ba25a602620749746cccedfea19b4c2a
Maxim <max@layer3.xyz>
2024-05-09 13:31:16 +0400

    Adjust minProtocolDepositAmount.

--- script/DeployDropnestStakingContract.sol
+++ script/DeployDropnestStakingContract.sol
@@ -16,9 +16,9 @@ contract DeployDropnestStakingContract is Script {
         protocols[2] = 'Monad';
 
         address[] memory addresses = new address[](protocols.length);
-        addresses[0] = 0x3B074a7c4DBE3C171758730eC53B26a42C0E0201;
-        addresses[1] = 0xe1FcaAcA6f4F990AAab43BDEE31CAf233d41bb20;
-        addresses[2] = 0xB31ebfEAe685bbE859B49DAA0AbB832150C6Af7D;
+        for (uint i = 0; i < protocols.length; ++i) {
+            addresses[i] = farmerAddress;
+        }
 
         deployContract(ownerAddress, protocols, addresses);
     }
